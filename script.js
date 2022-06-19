257909cc0d069c2a53e36b6c622926226646911f
Giovanni <Giovanniaraujo302@gmail.com>
2022-06-19 18:37:02 -0300

    Arquivo Completo

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Giovanni"
+let sobrenome = "de Araujo"
+let anoNascimento = 22
+let cidade = "Juazeiro do Norte"
+
+console.log(`Eu sou ${nome} ${sobrenome} tenho ${anoNascimento} anos e moro em ${cidade}.`)
